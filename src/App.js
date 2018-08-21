0ba1738c16bf2eec9f2bc8db38469881458d0883
Gray Smith <gray.smith00@gmail.com>
2018-08-21 14:34:17 -0600

    Dot then and async await solutions

--- src/App.js
+++ src/App.js
@@ -12,8 +12,58 @@ class App extends Component {
     };
   }
 
+  async componentDidMount() {
+    const url = 'http://localhost:3001/api/frontend-staff';
+    await this.getStaffInfo(url);
+  }
+
+  getStaffInfo = async url => {
+    try {
+      const response = await fetch(url);
+      const staffMembers = await response.json();
+      const staff = await this.populateMemberData(staffMembers.bio);
+      this.setState({ staff });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  populateMemberData = staffMembers => {
+    const unresolvedPromises = staffMembers.map(async member => {
+      const response = await fetch(member.info);
+      const memberInfo = await response.json();
+      return { ...memberInfo, name: member.name };
+    });
+    return Promise.all(unresolvedPromises);
+  };
+
+  // componentDidMount() {
+  //   const url = 'http://localhost:3001/api/frontend-staff';
+  //   this.getStaffInfo(url).then(staff =>
+  //     this.setState({
+  //       staff
+  //     })
+  //   );
+  // }
+
+  // getStaffInfo = url => {
+  //   return fetch(url)
+  //     .then(res => res.json())
+  //     .then(staffMembers => this.populateMemberData(staffMembers.bio))
+  //     .catch(error => console.log(error.message));
+  // };
+
+  // populateMemberData = staffMembers => {
+  //   const pendingPromises = staffMembers.map(member => {
+  //     return fetch(member.info)
+  //       .then(res => res.json())
+  //       .then(memberInfo => ({ ...memberInfo, name: member.name }));
+  //   });
+  //   return Promise.all(pendingPromises);
+  // };
+
   render() {
-    const { staff } = this.state
+    const { staff } = this.state;
 
     return (
       <div className="App">
@@ -22,12 +72,8 @@ class App extends Component {
           <h2>Front-End Staff</h2>
         </div>
         <div className="App-intro">
-          <div className='staff'>
-            {
-              !staff.length ?
-              <Loader /> :
-              <StaffList staff={staff} />
-            }
+          <div className="staff">
+            {!staff.length ? <Loader /> : <StaffList staff={staff} />}
           </div>
         </div>
       </div>
